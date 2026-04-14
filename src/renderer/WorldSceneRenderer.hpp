1e7ca7c81ce83be8e0579d3ea9531d00e2d0eef5
Benjamin Sutas <benjamin.sutas@gmail.com>
2026-04-14 16:32:26 +1000

    add light handles

--- src/renderer/WorldSceneRenderer.hpp
+++ src/renderer/WorldSceneRenderer.hpp
@@ -25,13 +25,25 @@ public:
     void beginFrame() override;
 
 private:
+    struct LightGizmoDragState {
+        bool active{false};
+        bool pointLight{true};
+        std::size_t lightIndex{0};
+        int axisIndex{0};
+        std::array<float, 3> startPosition{};
+        float startAxisParameter{0.0F};
+    };
+
     void drawChunkWireframeOverlay();
+    void drawLightGizmoOverlay();
+    void clearActiveLightDrag() noexcept;
     [[nodiscard]] float residentChunkBlend() const noexcept;
 
     VulkanContext* m_context{nullptr};
     RenderStateStore* m_renderStateStore{nullptr};
     RenderStateSnapshot m_renderStateSnapshot;
     bool m_chunkWireframeEnabled{false};
+    LightGizmoDragState m_activeLightDrag;
 };
 
 } // namespace Meridian
